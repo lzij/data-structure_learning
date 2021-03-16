d1c43624be4829db19743b0cd5aafdf71ad890ad
lzij <80672359+lzij@users.noreply.github.com>
2021-03-16 20:47:16 +0800

    Create bubblesorts.java
    冒泡排序法、插入排序法

--- data_structure/sorts/bubblesorts.java
+++ data_structure/sorts/bubblesorts.java
@@ -22,25 +22,19 @@ public class Sorts {
         return a;
     }
     
-    
-    //设置一个动态边界
-    public static int[] bubbleSort1(int[] a,int n){
+    //插入排序
+        public static int[] insertSort(int[]a,int n){
         if(n<=1) return a;
-        int lastexchange = 0;
-        int sortBorder =n-1;
-        for(int i =0;i<n;i++){
-            boolean flag = false;
-            for(int j = 0;j<sortBorder;j++){
-                if(a[j] >a[j+1]){
-                    int temp = a[j];
-                    a[j] = a[j+1];
-                    a[j+1] = temp;
-                    flag = true;
-                    lastexchange = j;
+        for(int i = 1;i<n;++i){
+            int value = a[i];
+            int j = i-1;
+            for(;j>=0;--j){
+                if(a[j] > value){
+                    a[j+1] = a[j];
                 }
+                else break;
             }
-            sortBorder =lastexchange;
-            if(!flag) break;
+            a[j+1] = value;
         }
         return a;
     }
